6ab42be574e028db41d6ba63cee46b03264749ea
Vincenzo Aprile <aprile.vincenzo202@gmail.com>
2024-02-16 14:45:37 +0100

    Initial Commit

---
+++ LostApples/LostApplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LostApplesApp.swift
+//  LostApples
+//
+//  Created by Vincenzo Aprile on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LostApplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
